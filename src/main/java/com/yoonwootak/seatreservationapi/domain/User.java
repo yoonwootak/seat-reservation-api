b1a1ec4b86260bf3509ff14d719828ec95e55d9c
yoonwootak <ywt0100@naver.com>
2026-02-27 03:25:09 +0900

    User 엔티티 생성 및 JPA 매핑 추가

---
+++ src/main/java/com/yoonwootak/seatreservationapi/domain/User.java
@@ -0,0 +1,22 @@
+package com.yoonwootak.seatreservationapi.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String passwordHash;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String role = "USER";
+}
