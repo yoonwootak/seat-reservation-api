15ff9afb9f7f1c65b9c9a8f76272ec6ccd3fcb1d
yoonwootak <ywt0100@naver.com>
2026-02-27 04:27:53 +0900

    Lombok 적용 및 getter, setter 추가

--- src/main/java/com/yoonwootak/seatreservationapi/domain/Event.java
+++ src/main/java/com/yoonwootak/seatreservationapi/domain/Event.java
@@ -1,9 +1,13 @@
 package com.yoonwootak.seatreservationapi.domain;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "events")
 public class Event {
