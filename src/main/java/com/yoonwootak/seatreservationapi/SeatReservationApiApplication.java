723eb23966d05eb04e03b34cb7bdcc5cfdb1dc82
yoonwootak <ywt0100@naver.com>
2026-03-02 01:24:28 +0900

    Gradle 빌드 환경 복구 및 DB 설정 보안 분리

---
+++ src/main/java/com/yoonwootak/seatreservationapi/SeatReservationApiApplication.java
@@ -0,0 +1,13 @@
+package com.yoonwootak.seatreservationapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeatReservationApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeatReservationApiApplication.class, args);
+    }
+
+}
