df2af872b43cce5b5e86799d041a2cb1c035073e
yoonwootak <ywt0100@naver.com>
2026-03-04 18:30:28 +0900

    좌석 선점 만료 자동 해제 스케줄러 추가

--- src/main/java/com/yoonwootak/seatreservationapi/repository/SeatRepository.java
+++ src/main/java/com/yoonwootak/seatreservationapi/repository/SeatRepository.java
@@ -1,11 +1,13 @@
 package com.yoonwootak.seatreservationapi.repository;
 
 import com.yoonwootak.seatreservationapi.domain.Seat;
+import com.yoonwootak.seatreservationapi.domain.SeatStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface SeatRepository extends JpaRepository<Seat, Long> {
     List<Seat> findBySectionId(Long sectionId);
+    List<Seat> findByStatusAndHoldExpiresAtBefore(SeatStatus status, LocalDateTime time);
 }
